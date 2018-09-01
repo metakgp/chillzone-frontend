d25a6dc5355ebf206eb47bbea00b9270f952121f
Siddharth Kannan <mail@siddharthkannan.in>
2018-09-01 15:06:43 +0530

    change timings to include only start time

--- src/Constants.js
+++ src/Constants.js
@@ -1,13 +1,13 @@
 export const Slots = [
-  "8 am - 9 am",
-  "9 am - 10 am",
-  "10 am - 11 am",
-  "11 am - 12 pm",
-  "12 pm - 1 pm",
-  "2 pm - 3 pm",
-  "3 pm - 4 pm",
-  "4 pm - 5 pm",
-  "5 pm - 6 pm",
+  "8 am",
+  "9 am ",
+  "10 am ",
+  "11 am ",
+  "12 pm",
+  "2 pm",
+  "3 pm",
+  "4 pm",
+  "5 pm",
 ];
 export const DayNames = [
   'Monday',
